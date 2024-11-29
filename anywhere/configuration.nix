76371e32c49a2e42057d36a5a649b87b1ef32932
= <camponogara5@gmail.com>
2024-11-28 22:27:18 -0300

    get nixos example

--- anywhere/configuration.nix
+++ anywhere/configuration.nix
@@ -25,7 +25,7 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     # change this to your ssh key
-    "CHANGE_ME"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQD1Ls2/4TEEiwe/16JOuwjkWylpJTMQnDMm1Ne+Dh2zO34EE26Xum2o+5+9cYaJB8NgmIc1rzhxJOF3eumzRZdyeqsH+cQyNZ8dMkWsAatApz/ZOKOJOx/huNfuM9zb5jZ6aG9Yn5rzX1ce/m+RM/DUk48QUmDfVZusL2QyaLfOvEIIS0OFxoBCLdctdspuoCmLi8ZNZaFUfhtBE8zKG984G7dmY1el5wxqQE0LFFj2k3XOir1p86QB5cP/TMnWgokCZ7YdUOGfQzUyXbBk6ObxoRWKb7y4yowuqjZR05cKeoEbWpe2ciTCQRjdjHBy9l1L+YbvD+kRejn/HydASI/70sPwdDG0rbFgflm/qaqPK/6zHcWcq8B/TWJSaQzbIHIEdUEUjmAE1w7hqbkUGKbc6682ZmzsN+TQsZBs7/cSxiAxGQ5Ay2r7dDAhbrB0FifX3Xw1U2NEkqh6d6k6wYx8LFIK2rrZ8t5A9tZefh+hzvzN2XxO1Hghrr15LHIVtq0= enric@avell"
   ];
 
   networking.firewall.allowedTCPPorts = [
